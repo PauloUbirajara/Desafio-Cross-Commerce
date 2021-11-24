f7e1d79ea8a767b5fbfb211e7a894b50f04abf24
Paulo Ubirajara de Mattos Neto <ubirajara3@hotmail.com>
2021-11-24 17:05:45 -0300

    Disponibilizar números ordenados no endpoint '/api'

--- src/app.py
+++ src/app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from api_controller import APIController
+from merge_sort import sort_array_by_merge_sort
 
 app = Flask(__name__)
+controller = APIController()
+
 
 @app.route("/api")
 def show_sorted_data():
-    return "<p>Hello, World!</p>"
+    number_list = controller.get_all_page_numbers_as_array()
+    sorted_number_list = sort_array_by_merge_sort(number_list)
+
+    result = {
+        'numbers': sorted_number_list
+    }
+
+    return result
